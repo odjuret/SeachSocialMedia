389e672c213fb659031d5707dbeb8bc65fb51f3f
Jimmie <sundberg.jimmie@gmail.com>
2023-07-04 17:17:51 +0200

    Initial commit

---
+++ src/main/java/com/github/odjuret/SearchSocialMedia/SearchSocialMediaApplication.java
@@ -0,0 +1,13 @@
+package com.github.odjuret.SearchSocialMedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchSocialMediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchSocialMediaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/odjuret/SearchSocialMedia/SearchSocialMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.odjuret.SearchSocialMedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchSocialMediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
